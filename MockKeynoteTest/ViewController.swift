086340c8066bbde43c79a9e228370d2703356ab8
廖冠翰 <henry@macbook-pro-2.local>
2018-09-30 11:11:30 +0800

    Initial Commit

---
+++ MockKeynoteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockKeynoteTest
+//
+//  Created by 廖冠翰 on 2018/9/30.
+//  Copyright © 2018年 廖冠翰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MockKeynoteTestTests/MockKeynoteTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MockKeynoteTestTests.swift
+//  MockKeynoteTestTests
+//
+//  Created by 廖冠翰 on 2018/9/30.
+//  Copyright © 2018年 廖冠翰. All rights reserved.
+//
+
+import XCTest
+@testable import MockKeynoteTest
+
+class MockKeynoteTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
